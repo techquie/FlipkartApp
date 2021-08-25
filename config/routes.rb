ede6f777f3f0e5b08d45cdd43fb6bee0d61cc679
techquie <kundanverma265@gmail.com>
2021-08-25 16:56:28 +0530

    commit 2

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
 
+  resources :recipes
   root "home#index"
 
   get "flipkart", to: "home#index"
+
+  #get "/recipes/new", to: "recipe#new"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
